fe60b93ea08c395fdaa5cfd3f1832ac5ab26b5cb
khushab <khushabalam7@gmail.com>
2021-08-08 13:29:19 +0530

    login submit handler added

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,19 +1,44 @@
-import React from 'react'
-import './login.scss'
+import React, { useState } from 'react'
+import './Login.scss'
 
 const Login = () => {
+
+    const [email, setEmail] = useState('')
+    const [password, setPassword] = useState('')
+    const submitHandler = (e) => {
+        e.preventDefault()
+        const formData = {
+            email: email,
+            password: password
+        }
+
+        console.log(formData)
+    }
+
     return (
         <div className="login">
             <h2>Task Manager</h2>
-            <form className="login-form">
+            <form onSubmit={submitHandler} className="login-form">
                 <div className="form-group">
-                    <input type="text" placeholder="Email" />
+                    <input
+                        type="email"
+                        placeholder="Email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                        required
+                    />
                 </div>
                 <div className="form-group">
-                    <input type="password" placeholder="Password" />
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        value={password}
+                        onChange={e => setPassword(e.target.value)}
+                        required
+                    />
                 </div>
                 <div className="form-group">
-                    <button>Login</button>
+                    <button type='submit'>Login</button>
                 </div>
                 <div className="line"></div>
                 <div className="form-group">

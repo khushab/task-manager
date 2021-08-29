67f96fef214f290207251a25f20fe8f931a8eeda
khushab <khushabalam7@gmail.com>
2021-08-29 14:27:42 +0530

    home component  created

---
+++ src/pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import './Home.scss'
+import Nav from '../../components/navbar/Nav'
+
+const Home = () => {
+    return (
+        <div className="home">
+            <Nav />
+        </div>
+    )
+}
+
+export default Home
